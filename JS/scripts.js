cea5dea06d9eea06bf177279b9afedb64cc34b17
LoulouLinck <136842839+LoulouLinck@users.noreply.github.com>
2023-07-25 01:16:09 +0200

    function crossOut and deleteListItem

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,18 @@
+// jQuery no equivalent to createElement: append HTML as a string: $('.selectedElement').append('<li>some item</li>').
+// Can reuse createTextNode('X') in jQuery: deleteButton.append(document.createTextNode('X'));.
+
+function newItem(){
+
+//1. Adding a new item to the list of items: 
+   let li = $('<li></li>');
+   let inputValue = $('#input').val();
+   li.append(inputValue);
+
+   if (inputValue === '') {
+     alert('You must write something!');
+   } else {
+     $('#list').append(li);
+   }
 
  //2. Crossing out an item from the list of items:
   function crossOut() {
